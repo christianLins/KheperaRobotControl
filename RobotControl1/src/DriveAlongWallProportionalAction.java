7b979c356d4ca50484ac0f4c8f09e425d7d8ea08
Chris <christian.lins87@gmail.com>
2012-12-14 13:24:26 +0100

    update

--- RobotControl1/src/DriveAlongWallProportionalAction.java
+++ RobotControl1/src/DriveAlongWallProportionalAction.java
@@ -1,6 +1,14 @@
 
 public class DriveAlongWallProportionalAction extends Action {
 
+	
+	private float[][] controlMatrix = new float[][] { 
+			new float[] { 0.0f,	 6.50f, 60f, 	60f, 	60f, 	60f, 	-60f, 	-60f	},
+			new float[] { 6.25f, 0.00f,	-6.50f, -6.50f, -6.50f, -6.50f, 0.00f, 	0f 		} 
+			}; 
+	private float PROP_BASE_SPEED = 5;
+	private final int MAX_DISTANCE_VALUE = 1023;
+	
 	public DriveAlongWallProportionalAction(ActionContext context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -8,7 +16,21 @@ public class DriveAlongWallProportionalAction extends Action {
 
 	@Override
 	public void doAction() {
-		// TODO Auto-generated method stub
+		float left = 0;
+		float right = 0;
+		
+		float[] sensors = getSensorManager().getSensorDistanceVector();
+
+		for (int i = 0; i < sensors.length; i++) {
+			left += controlMatrix[0][i] * sensors[i];
+			right += controlMatrix[1][i] * sensors[i];
+		}
+
+		left = left / MAX_DISTANCE_VALUE;
+		right = right / MAX_DISTANCE_VALUE;
+		
+		getMotionManager().setMotorSpeeds((int)(PROP_BASE_SPEED * 0 + (left) * PROP_BASE_SPEED), (int)(PROP_BASE_SPEED * 0 + (right)* PROP_BASE_SPEED));
+		
 
 	}
 
